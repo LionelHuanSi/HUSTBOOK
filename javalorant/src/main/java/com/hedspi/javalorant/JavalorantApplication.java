e7369231726eefc3ca1ad40919457c3b2e07f3f6
TaDinhThien05 <thien.td235839@sis.hust.edu.vn>
2025-05-16 21:28:43 +0700

    new spring app

---
+++ javalorant/src/main/java/com/hedspi/javalorant/JavalorantApplication.java
@@ -0,0 +1,13 @@
+package com.hedspi.javalorant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavalorantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavalorantApplication.class, args);
+	}
+
+}
